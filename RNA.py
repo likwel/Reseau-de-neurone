16a82dfa15c284244f49d979b35e0e4aca47b35d
Elie <eliefenohasina@gmail.com>
2023-03-23 13:58:36 +0300

    2nd commit

--- RNA.py
+++ RNA.py
@@ -4,47 +4,54 @@ from sklearn.datasets import make_blobs, make_circles
 from sklearn.metrics import accuracy_score, log_loss
 from tqdm import tqdm
 
+
 def initialisation(dimensions):
-    
+
     parametres = {}
     C = len(dimensions)
 
     np.random.seed(1)
 
     for c in range(1, C):
-        parametres['W' + str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
+        parametres['W' +
+                   str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
         parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
 
     return parametres
 
+
 def forward_propagation(X, parametres):
-  
-  activations = {'A0': X}
 
-  C = len(parametres) // 2
+    activations = {'A0': X}
 
-  for c in range(1, C + 1):
+    C = len(parametres) // 2
 
-    Z = parametres['W' + str(c)].dot(activations['A' + str(c - 1)]) + parametres['b' + str(c)]
-    activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+    for c in range(1, C + 1):
 
-  return activations
+        Z = parametres['W' + str(c)].dot(activations['A' +
+                                                     str(c - 1)]) + parametres['b' + str(c)]
+        activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
+
+    return activations
+
 
 def back_propagation(y, parametres, activations):
 
-  m = y.shape[1]
-  C = len(parametres) // 2
+    m = y.shape[1]
+    C = len(parametres) // 2
 
-  dZ = activations['A' + str(C)] - y
-  gradients = {}
+    dZ = activations['A' + str(C)] - y
+    gradients = {}
 
-  for c in reversed(range(1, C + 1)):
-    gradients['dW' + str(c)] = 1/m * np.dot(dZ, activations['A' + str(c - 1)].T)
-    gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
-    if c > 1:
-      dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' + str(c - 1)] * (1 - activations['A' + str(c - 1)])
+    for c in reversed(range(1, C + 1)):
+        gradients['dW' + str(c)] = 1/m * np.dot(dZ,
+                                                activations['A' + str(c - 1)].T)
+        gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
+        if c > 1:
+            dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' +
+                                                                      str(c - 1)] * (1 - activations['A' + str(c - 1)])
 
-  return gradients
+    return gradients
 
 
 def update(gradients, parametres, learning_rate):
@@ -52,19 +59,23 @@ def update(gradients, parametres, learning_rate):
     C = len(parametres) // 2
 
     for c in range(1, C + 1):
-        parametres['W' + str(c)] = parametres['W' + str(c)] - learning_rate * gradients['dW' + str(c)]
-        parametres['b' + str(c)] = parametres['b' + str(c)] - learning_rate * gradients['db' + str(c)]
+        parametres['W' + str(c)] = parametres['W' + str(c)] - \
+            learning_rate * gradients['dW' + str(c)]
+        parametres['b' + str(c)] = parametres['b' + str(c)] - \
+            learning_rate * gradients['db' + str(c)]
 
     return parametres
 
-def predict(X, parametres):
-  activations = forward_propagation(X, parametres)
-  C = len(parametres) // 2
-  Af = activations['A' + str(C)]
-  return Af >= 0.5
 
-def deep_neural_network(X, y, hidden_layers = (16, 16, 16), learning_rate = 0.001, n_iter = 3000):
-    
+def predict(X, parametres):
+    activations = forward_propagation(X, parametres)
+    C = len(parametres) // 2
+    Af = activations['A' + str(C)]
+    return Af >= 0.5
+
+
+def deep_neural_network(X, y, hidden_layers=(16, 16, 16), learning_rate=0.001, n_iter=3000):
+
     # initialisation parametres
     dimensions = list(hidden_layers)
     dimensions.insert(0, X.shape[0])
@@ -88,7 +99,8 @@ def deep_neural_network(X, y, hidden_layers = (16, 16, 16), learning_rate = 0.00
         # calcul du log_loss et de l'accuracy
         training_history[i, 0] = (log_loss(y.flatten(), Af.flatten()))
         y_pred = predict(X, parametres)
-        training_history[i, 1] = (accuracy_score(y.flatten(), y_pred.flatten()))
+        training_history[i, 1] = (
+            accuracy_score(y.flatten(), y_pred.flatten()))
 
     # Plot courbe d'apprentissage
     plt.figure(figsize=(12, 4))
@@ -102,6 +114,7 @@ def deep_neural_network(X, y, hidden_layers = (16, 16, 16), learning_rate = 0.00
 
     return training_history
 
+
 X, y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
 X = X.T
 y = y.reshape((1, y.shape[0]))
@@ -112,5 +125,6 @@ print('dimensions de y:', y.shape)
 plt.scatter(X[0, :], X[1, :], c=y, cmap='summer')
 plt.show()
 
-#appel fonction
-deep_neural_network(X, y, hidden_layers = (16, 16, 16), learning_rate = 0.1, n_iter = 3000)
+# appel fonction
+deep_neural_network(X, y, hidden_layers=(16, 16, 16),
+                    learning_rate=0.1, n_iter=3000)
