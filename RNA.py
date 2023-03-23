d1957e230d9005fb422a22f74f51317b43b1b61c
Elie <eliefenohasina@gmail.com>
2023-03-23 14:00:24 +0300

    3rd commit

--- RNA.py
+++ RNA.py
@@ -5,40 +5,40 @@ from sklearn.metrics import accuracy_score, log_loss
 from tqdm import tqdm
 
 
-def initialisation(dimensions):
+def initialisation(dim):
 
-    parametres = {}
-    C = len(dimensions)
+    params = {}
+    C = len(dim)
 
     np.random.seed(1)
 
     for c in range(1, C):
-        parametres['W' +
-                   str(c)] = np.random.randn(dimensions[c], dimensions[c - 1])
-        parametres['b' + str(c)] = np.random.randn(dimensions[c], 1)
+        params['W' +
+                   str(c)] = np.random.randn(dim[c], dim[c - 1])
+        params['b' + str(c)] = np.random.randn(dim[c], 1)
 
-    return parametres
+    return params
 
 
-def forward_propagation(X, parametres):
+def forward_propagation(X, params):
 
     activations = {'A0': X}
 
-    C = len(parametres) // 2
+    C = len(params) // 2
 
     for c in range(1, C + 1):
 
-        Z = parametres['W' + str(c)].dot(activations['A' +
-                                                     str(c - 1)]) + parametres['b' + str(c)]
+        Z = params['W' + str(c)].dot(activations['A' +
+                                                     str(c - 1)]) + params['b' + str(c)]
         activations['A' + str(c)] = 1 / (1 + np.exp(-Z))
 
     return activations
 
 
-def back_propagation(y, parametres, activations):
+def back_propagation(y, params, activations):
 
     m = y.shape[1]
-    C = len(parametres) // 2
+    C = len(params) // 2
 
     dZ = activations['A' + str(C)] - y
     gradients = {}
@@ -48,67 +48,67 @@ def back_propagation(y, parametres, activations):
                                                 activations['A' + str(c - 1)].T)
         gradients['db' + str(c)] = 1/m * np.sum(dZ, axis=1, keepdims=True)
         if c > 1:
-            dZ = np.dot(parametres['W' + str(c)].T, dZ) * activations['A' +
+            dZ = np.dot(params['W' + str(c)].T, dZ) * activations['A' +
                                                                       str(c - 1)] * (1 - activations['A' + str(c - 1)])
 
     return gradients
 
 
-def update(gradients, parametres, learning_rate):
+def update(gradients, params, learning_rate):
 
-    C = len(parametres) // 2
+    C = len(params) // 2
 
     for c in range(1, C + 1):
-        parametres['W' + str(c)] = parametres['W' + str(c)] - \
+        params['W' + str(c)] = params['W' + str(c)] - \
             learning_rate * gradients['dW' + str(c)]
-        parametres['b' + str(c)] = parametres['b' + str(c)] - \
+        params['b' + str(c)] = params['b' + str(c)] - \
             learning_rate * gradients['db' + str(c)]
 
-    return parametres
+    return params
 
 
-def predict(X, parametres):
-    activations = forward_propagation(X, parametres)
-    C = len(parametres) // 2
+def predict(X, params):
+    activations = forward_propagation(X, params)
+    C = len(params) // 2
     Af = activations['A' + str(C)]
     return Af >= 0.5
 
 
 def deep_neural_network(X, y, hidden_layers=(16, 16, 16), learning_rate=0.001, n_iter=3000):
 
-    # initialisation parametres
-    dimensions = list(hidden_layers)
-    dimensions.insert(0, X.shape[0])
-    dimensions.append(y.shape[0])
+    # initialisation params
+    dim = list(hidden_layers)
+    dim.insert(0, X.shape[0])
+    dim.append(y.shape[0])
     np.random.seed(1)
-    parametres = initialisation(dimensions)
+    params = initialisation(dim)
 
     # tableau numpy contenant les futures accuracy et log_loss
     training_history = np.zeros((int(n_iter), 2))
 
-    C = len(parametres) // 2
+    C = len(params) // 2
 
     # gradient descent
     for i in tqdm(range(n_iter)):
 
-        activations = forward_propagation(X, parametres)
-        gradients = back_propagation(y, parametres, activations)
-        parametres = update(gradients, parametres, learning_rate)
+        activations = forward_propagation(X, params)
+        gradients = back_propagation(y, params, activations)
+        params = update(gradients, params, learning_rate)
         Af = activations['A' + str(C)]
 
         # calcul du log_loss et de l'accuracy
         training_history[i, 0] = (log_loss(y.flatten(), Af.flatten()))
-        y_pred = predict(X, parametres)
+        y_pred = predict(X, params)
         training_history[i, 1] = (
             accuracy_score(y.flatten(), y_pred.flatten()))
 
     # Plot courbe d'apprentissage
     plt.figure(figsize=(12, 4))
     plt.subplot(1, 2, 1)
-    plt.plot(training_history[:, 0], label='train loss')
+    plt.plot(training_history[:, 0], label='Train loss')
     plt.legend()
     plt.subplot(1, 2, 2)
-    plt.plot(training_history[:, 1], label='train acc')
+    plt.plot(training_history[:, 1], label='Train acc')
     plt.legend()
     plt.show()
 
@@ -119,8 +119,8 @@ X, y = make_circles(n_samples=100, noise=0.1, factor=0.3, random_state=0)
 X = X.T
 y = y.reshape((1, y.shape[0]))
 
-print('dimensions de X:', X.shape)
-print('dimensions de y:', y.shape)
+print('Dim de X:', X.shape)
+print('Dim de y:', y.shape)
 
 plt.scatter(X[0, :], X[1, :], c=y, cmap='summer')
 plt.show()
